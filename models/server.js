7f37fe2d9acd6f301934bb6fc37f627784e68039
Uriel Vargas <urielvc22@gmail.com>
2023-09-07 14:13:19 -0400

    1er coo

--- models/server.js
+++ models/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 
 class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios'
 
         // Middlewares
         this.middlewares();
@@ -12,36 +14,19 @@ class Server{
         this.routes();
     }
     middlewares(){
+
+        //CORS
+        this.app.use(cors());
+
+        // Lectura y parseo del body
+        this.app.use( express.json());
+
         // Directorios publicos
         this.app.use(express.static('public'));
     }
 
     routes() {
-        this.app.get('/api', (req,res) =>{
-            res.json({
-                msg: 'get API'
-            });
-        });
-        this.app.put('/api', (req,res) =>{
-            res.json({
-                msg: 'put API'
-            });
-        });
-        this.app.post('/api', (req,res) =>{
-            res.json({
-                msg: 'post API'
-            });
-        });
-        this.app.delete('/api', (req,res) =>{
-            res.json({
-                msg: 'delete API'
-            });
-        });
-        this.app.patch('/api', (req,res) =>{
-            res.json({
-                msg: 'patch API'
-            });
-        });
+        this.app.use('/api/usuarios', require('../routes/usuarios'))
     }
     listen(){
         this.app.listen(this.port, () =>{
